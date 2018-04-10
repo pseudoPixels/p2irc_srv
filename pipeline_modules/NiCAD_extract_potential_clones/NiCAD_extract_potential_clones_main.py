8075d91ec1be3c9baf37f8a1b03f62ab2d920c21
Golam Mostaeen (gom766) <golam.mostaeen@usask.ca>
2018-04-09 21:13:02 -0600

    New Software System added for Source Analysis

--- pipeline_modules/NiCAD_extract_potential_clones/NiCAD_extract_potential_clones_main.py
+++ pipeline_modules/NiCAD_extract_potential_clones/NiCAD_extract_potential_clones_main.py
@@ -3,7 +3,7 @@ import subprocess
 
 pipe = subprocess.Popen(
     ["/bin/bash", "/home/ubuntu/Webpage/app_collaborative_sci_workflow/External_Libraries/NiCad-4.0/scripts/Extract",
-     "functions", "java",
-     '/home/ubuntu/Webpage/app_collaborative_sci_workflow/Dataset/open_source_projects/JHotDraw54b1', "", "",
+     granularity, language,
+     system_directory, select_pattern, ignore_pattern,
      "/home/ubuntu/Webpage/app_collaborative_sci_workflow/workflow_outputs/test_workflow/"]).communicate()
 
---
+++ pipeline_modules/NiCAD_extract_potential_clones/NiCAD_extract_potential_clones_settings.py
@@ -0,0 +1,7 @@
+
+
+granularity = 'functions'
+language = 'java'
+system_directory='/home/ubuntu/Webpage/app_collaborative_sci_workflow/Dataset/open_source_projects/JHotDraw54b1'
+select_pattern=''
+ignore_pattern=''
