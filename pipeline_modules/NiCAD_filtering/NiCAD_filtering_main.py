a5319bbaa09e83121e9140d91215463fd803dea0
Golam Mostaeen (gom766) <golam.mostaeen@usask.ca>
2018-04-25 16:20:07 -0600

    Tool Updated according to New node Sturcture

--- pipeline_modules/NiCAD_filtering/NiCAD_filtering_main.py
+++ pipeline_modules/NiCAD_filtering/NiCAD_filtering_main.py
@@ -4,6 +4,6 @@ import subprocess
 pipe = subprocess.Popen(
     ["/bin/bash", "/home/ubuntu/Webpage/app_collaborative_sci_workflow/External_Libraries/NiCad-4.0/scripts/Filter",
      granularity, language,
-     input_source, filters,
-     output_destination]).communicate()
+     potential_clones, filters,
+     filtered_potential_clones]).communicate()
 
--- pipeline_modules/NiCAD_normalizing/NiCAD_normalizing_main.py
+++ pipeline_modules/NiCAD_normalizing/NiCAD_normalizing_main.py
@@ -4,6 +4,6 @@ import subprocess
 pipe = subprocess.Popen(
     ["/bin/bash", "/home/ubuntu/Webpage/app_collaborative_sci_workflow/External_Libraries/NiCad-4.0/scripts/Normalize",
      granularity, language,
-     input_source, normalization,
-     output_destination]).communicate()
+     potential_clones, normalization,
+     normalized_potential_clones]).communicate()
 
